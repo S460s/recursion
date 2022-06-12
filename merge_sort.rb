8de0a12998eee687d5d4f91e790dc51154c0b6a6
S460 <rommel.e@protonmail.ch>
2022-06-12 16:36:14 +0300

    readability

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,7 +2,7 @@ def merge_sorted_array(a, b)
   sorted = []
 
   loop do
-    sorted << (a[0] < b[0] ? a.shift : b.shift)
+    sorted << (a.first < b.first ? a.shift : b.shift)
     return sorted + a if b.empty?
     return sorted + b if a.empty?
   end
@@ -11,7 +11,11 @@ end
 def merge(arr)
   return arr if arr.length < 2
 
-  merge_sorted_array(merge(arr.slice(0, arr.length / 2)), merge(arr.slice(arr.length / 2, arr.length)))
+  mid = arr.length / 2
+  left = arr.slice(0, mid)
+  right = arr.slice(mid, arr.length)
+
+  merge_sorted_array(merge(left), merge(right))
 end
 
-p merge [4, 3, 2, 1]
+p merge [4, 3, 2, 1, 1]
